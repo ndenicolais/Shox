75a766489ab41ca71ccec24b038cb3e50d5a49e1
ndenicolais <ndn21dev@gmail.com>
2024-07-12 21:47:21 +0200

    Shoes model

---
+++ lib/shoes/shoes_model.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Shoes {
+  String? id;
+  bool isFavorite;
+  String imageUrl;
+  Color color;
+  IconData? seasonIcon;
+  static List<IconData> seasonOptions = [
+    Icons.sunny,
+    Icons.ac_unit,
+    Icons.clear
+  ];
+  String brand;
+  String size;
+  String category;
+  static List<String> categoryOptions = [
+    'Trainers',
+    'Classic',
+    'Work',
+    'Sport',
+    'Other'
+  ];
+  String type;
+  static List<String> typeOptions = [
+    'Sneakers',
+    'Heels',
+    'Flat Sandals',
+    'Heeled Sandals',
+    'Boots',
+    'Hogan',
+    'Other'
+  ];
+  String? notes;
+  DateTime dateAdded;
+
+  Shoes({
+    this.id,
+    this.isFavorite = false,
+    required this.imageUrl,
+    required this.color,
+    this.seasonIcon,
+    required this.brand,
+    required this.size,
+    required this.category,
+    required this.type,
+    this.notes,
+    DateTime? dateAdded,
+  }) : dateAdded = dateAdded ?? DateTime.now();
+
+  // Convert Shoes object to Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'isFavorite': isFavorite,
+      'imageUrl': imageUrl,
+      'color': color.value,
+      'seasonIcon':
+          seasonIcon != null ? seasonIcon!.codePoint : Icons.clear.codePoint,
+      'brand': brand,
+      'size': size,
+      'category': category,
+      'type': type,
+      'notes': notes,
+      'dateAdded': dateAdded,
+    };
+  }
+
+  // Create Shoes object from Map
+  factory Shoes.fromMap(Map<String, dynamic> map, String documentId) {
+    return Shoes(
+      id: documentId,
+      isFavorite: map['isFavorite'],
+      imageUrl: map['imageUrl'],
+      color: Color(map['color']),
+      seasonIcon: map['seasonIcon'] != null
+          ? IconData(map['seasonIcon'], fontFamily: 'MaterialIcons')
+          : Icons.clear,
+      brand: map['brand'],
+      size: map['size'],
+      category: map['category'],
+      type: map['type'],
+      notes: map['notes'],
+      dateAdded: (map['dateAdded'] as Timestamp).toDate(),
+    );
+  }
+}
