2a0dfc5fb7d5e4d0561d3e596d9bae818ad46256
ndenicolais <ndn21dev@gmail.com>
2024-07-20 15:27:30 +0200

    Update app colors

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color lightYellow = Color(0xFFFFFDF9);
-  static const Color redPeach = Color(0xFFDA7C72);
   static const Color smoothBlack = Color(0xFF3D3F4D);
+  static const Color darkPeach = Color(0xFFDA7C72);
+  static const Color lightPeach = Color(0xFFFFC4BE);
+  static const Color sun = Color(0xFFF4A261);
+  static const Color lightGrey = Color(0xFFE7E7E7);
   static const Color confirmColor = Color(0xFF449777);
   static const Color errorColor = Color(0xFFD80032);
-  static const Color lightGrey = Color(0xFFE7E7E7);
   static const Color white = Colors.white;
   static const Color black = Colors.black;
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -7,16 +7,17 @@ class AppTheme {
       colorScheme: const ColorScheme.light(
         primary: AppColors.lightYellow,
         onPrimary: AppColors.lightYellow,
-        secondary: AppColors.redPeach,
+        secondary: AppColors.darkPeach,
         onSecondary: AppColors.lightYellow,
         tertiary: AppColors.smoothBlack,
-        onTertiary: AppColors.redPeach,
-        surface: AppColors.lightYellow,
-        onSurface: AppColors.lightYellow,
+        onTertiary: AppColors.darkPeach,
+        surface: AppColors.lightPeach,
+        onSurface: AppColors.smoothBlack,
         onError: AppColors.errorColor,
       ),
       textSelectionTheme: const TextSelectionThemeData(
-        selectionHandleColor: AppColors.redPeach,
+        selectionColor: AppColors.lightPeach,
+        selectionHandleColor: AppColors.lightPeach,
       ),
     );
   }
@@ -24,18 +25,19 @@ class AppTheme {
   static ThemeData darkTheme() {
     return ThemeData(
       colorScheme: const ColorScheme.dark(
-        primary: AppColors.redPeach,
-        onPrimary: AppColors.redPeach,
+        primary: AppColors.smoothBlack,
+        onPrimary: AppColors.smoothBlack,
         secondary: AppColors.lightYellow,
-        onSecondary: AppColors.redPeach,
-        tertiary: AppColors.smoothBlack,
+        onSecondary: AppColors.smoothBlack,
+        tertiary: AppColors.lightYellow,
         onTertiary: AppColors.lightYellow,
-        surface: AppColors.redPeach,
-        onSurface: AppColors.redPeach,
+        surface: AppColors.lightYellow,
+        onSurface: AppColors.smoothBlack,
         onError: AppColors.errorColor,
       ),
       textSelectionTheme: const TextSelectionThemeData(
-        selectionHandleColor: AppColors.lightYellow,
+        selectionColor: AppColors.lightPeach,
+        selectionHandleColor: AppColors.lightPeach,
       ),
     );
   }
