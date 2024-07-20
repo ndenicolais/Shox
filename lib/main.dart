50a2d3e9025680a9a38d8ef7716cb335cee70759
ndenicolais <ndn21dev@gmail.com>
2024-07-20 15:30:59 +0200

    Add theme provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
 import 'package:shox/firebase_options.dart';
 import 'package:shox/pages/intro_page.dart';
 import 'package:shox/theme/app_theme.dart';
+import 'package:shox/theme/theme_notifier.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(AppTheme.lightTheme(), false),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,9 +25,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lightTheme(),
-      home: const IntroPage(),
+    return Consumer<ThemeNotifier>(
+      builder: (context, themeNotifier, child) {
+        final brightness = MediaQuery.of(context).platformBrightness;
+
+        if (!themeNotifier.isManualTheme) {
+          if (brightness == Brightness.dark && !themeNotifier.isDarkMode) {
+            themeNotifier.setDarkTheme();
+          } else if (brightness == Brightness.light &&
+              themeNotifier.isDarkMode) {
+            themeNotifier.setLightTheme();
+          }
+          themeNotifier.resetManualTheme();
+        }
+
+        return GetMaterialApp(
+          theme: themeNotifier.currentTheme,
+          home: const IntroPage(),
+        );
+      },
     );
   }
 }
