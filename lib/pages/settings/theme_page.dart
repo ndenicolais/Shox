147ef566ddca8c033e32ebf6eb174eeeada4bfd2
ndenicolais <ndn21dev@gmail.com>
2024-08-03 14:01:39 +0200

    Update page

--- lib/pages/settings/theme_page.dart
+++ lib/pages/settings/theme_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:ming_cute_icons/ming_cute_icons.dart';
 import 'package:provider/provider.dart';
+import 'package:shox/generated/l10n.dart';
 import 'package:shox/theme/app_colors.dart';
 import 'package:shox/theme/theme_notifier.dart';
 
@@ -22,7 +24,7 @@ class ThemePage extends StatelessWidget {
           },
         ),
         title: Text(
-          'Theme',
+          S.current.theme_title,
           style: TextStyle(
             color: Theme.of(context).colorScheme.tertiary,
             fontWeight: FontWeight.bold,
@@ -35,112 +37,107 @@ class ThemePage extends StatelessWidget {
       ),
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Icon(
-                MingCuteIcons.mgc_palette_fill,
-                size: 120,
-                color: Theme.of(context).colorScheme.secondary,
-              ),
-              const SizedBox(height: 80),
-              Text(
-                'Select the theme of the app\nby swiping between\nthe cards below',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  fontSize: 22,
-                  fontFamily: 'CustomFont',
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 30.r, horizontal: 30.r),
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/img_theme.png',
+                  width: 120.r,
+                  height: 120.r,
                 ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Card(
-                    color: AppColors.lightYellow,
-                    shape: RoundedRectangleBorder(
-                      side: const BorderSide(
-                          color: AppColors.smoothBlack, width: 1.0),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: InkWell(
-                      onTap: () {
+                40.verticalSpace,
+                Text(
+                  S.current.theme_description,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.tertiary,
+                    fontSize: 22.r,
+                    fontFamily: 'CustomFont',
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                40.verticalSpace,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildThemeCard(
+                      context,
+                      AppColors.lightYellow,
+                      MingCuteIcons.mgc_sun_fill,
+                      S.current.theme_light,
+                      () {
                         Provider.of<ThemeNotifier>(context, listen: false)
                             .setLightTheme();
                       },
-                      splashColor: Colors.transparent,
-                      child: Container(
-                        width: 150,
-                        height: 200,
-                        padding: const EdgeInsets.all(20.0),
-                        child: const Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              MingCuteIcons.mgc_sun_fill,
-                              size: 60,
-                              color: AppColors.sun,
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              'Light',
-                              style: TextStyle(
-                                color: AppColors.smoothBlack,
-                                fontSize: 16,
-                                fontFamily: 'CustomFont',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
                     ),
-                  ),
-                  const SizedBox(width: 10),
-                  Card(
-                    color: AppColors.smoothBlack,
-                    shape: RoundedRectangleBorder(
-                      side: const BorderSide(
-                          color: AppColors.lightYellow, width: 1.0),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: InkWell(
-                      onTap: () {
+                    10.horizontalSpace,
+                    _buildThemeCard(
+                      context,
+                      AppColors.smoothBlack,
+                      MingCuteIcons.mgc_moon_fill,
+                      S.current.theme_dark,
+                      () {
                         Provider.of<ThemeNotifier>(context, listen: false)
                             .setDarkTheme();
                       },
-                      splashColor: Colors.transparent,
-                      child: Container(
-                        width: 150,
-                        height: 200,
-                        padding: const EdgeInsets.all(20.0),
-                        child: const Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              MingCuteIcons.mgc_moon_fill,
-                              size: 60,
-                              color: AppColors.white,
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              'Dark',
-                              style: TextStyle(
-                                color: AppColors.white,
-                                fontSize: 16,
-                                fontFamily: 'CustomFont',
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildThemeCard(
+    BuildContext context,
+    Color backgroundColor,
+    IconData iconData,
+    String text,
+    VoidCallback onTap,
+  ) {
+    return Card(
+      color: backgroundColor,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+            color: backgroundColor == AppColors.lightYellow
+                ? AppColors.smoothBlack
+                : AppColors.lightYellow,
+            width: 1.0),
+        borderRadius: BorderRadius.circular(10.0.r),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        splashColor: Colors.transparent,
+        child: Container(
+          width: 150.r,
+          height: 200.r,
+          padding: EdgeInsets.all(20.0.r),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                iconData,
+                size: 60.r,
+                color: backgroundColor == AppColors.lightYellow
+                    ? AppColors.darkGold
+                    : AppColors.white,
+              ),
+              10.verticalSpace,
+              Text(
+                text,
+                style: TextStyle(
+                  color: backgroundColor == AppColors.lightYellow
+                      ? AppColors.smoothBlack
+                      : AppColors.white,
+                  fontSize: 16.r,
+                  fontFamily: 'CustomFont',
+                ),
               ),
             ],
           ),
