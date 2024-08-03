6d84b3f7f4b30cca7c61048199453b6eb6cc1a88
ndenicolais <ndn21dev@gmail.com>
2024-08-03 14:01:21 +0200

    Update page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shox/onboarding/onboarding_page.dart';
 import 'package:shox/pages/welcome_page.dart';
 import 'package:shox/pages/shoes/shoes_home.dart';
 
@@ -23,23 +25,32 @@ class IntroPageState extends State<IntroPage> {
     _checkRememberMe();
   }
 
-  // Check if the user has selected "Remember Me" and navigate accordingly
   Future<void> _checkRememberMe() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool rememberMe = prefs.getBool('remember_me') ?? false;
+    bool onboardingCompleted = prefs.getBool('onboarding_completed') ?? false;
 
-    if (rememberMe) {
+    if (!onboardingCompleted) {
       Get.to(
-        () => const ShoesHome(),
+        () => const OnboardingPage(),
         transition: Transition.fade,
         duration: const Duration(milliseconds: 500),
       );
     } else {
-      Get.to(
-        () => const WelcomePage(),
-        transition: Transition.fade,
-        duration: const Duration(milliseconds: 500),
-      );
+      bool rememberMe = prefs.getBool('remember_me') ?? false;
+
+      if (rememberMe) {
+        Get.to(
+          () => const ShoesHome(),
+          transition: Transition.fade,
+          duration: const Duration(milliseconds: 500),
+        );
+      } else {
+        Get.to(
+          () => const WelcomePage(),
+          transition: Transition.fade,
+          duration: const Duration(milliseconds: 500),
+        );
+      }
     }
   }
 
@@ -51,31 +62,19 @@ class IntroPageState extends State<IntroPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 30),
+            Image.asset(
+              'assets/images/app_logo.png',
+              width: 180.r,
+              height: 180.r,
+            ),
             Text(
               'Shox',
               style: TextStyle(
-                color: Theme.of(context).colorScheme.secondary,
-                fontSize: 100,
+                color: Theme.of(context).colorScheme.tertiary,
+                fontSize: 80.r,
                 fontWeight: FontWeight.bold,
                 fontFamily: 'CustomFont',
               ),
-            ),
-            const SizedBox(height: 50),
-            Image.asset(
-              'assets/images/app_logo.png',
-              width: 250,
-              height: 250,
-            ),
-            const SizedBox(height: 50),
-            Text(
-              'Your\nshoes\neverywhere',
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.secondary,
-                fontSize: 34,
-                fontFamily: 'CustomFont',
-              ),
-              textAlign: TextAlign.center,
             ),
           ],
         ),
