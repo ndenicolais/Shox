797c94a8ef35710e857980b00f8d31e023ef93ae
ndenicolais <ndn21dev@gmail.com>
2024-09-28 10:55:28 +0200

    Fixed app theme reset

--- lib/theme/theme_notifier.dart
+++ lib/theme/theme_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shox/theme/app_theme.dart';
 
 class ThemeNotifier with ChangeNotifier {
@@ -6,12 +7,16 @@ class ThemeNotifier with ChangeNotifier {
   bool _isDarkMode;
   bool _isManualTheme;
 
+  static const String _themePreferenceKey = "theme_preference";
+
   ThemeNotifier(this._currentTheme, this._isDarkMode) : _isManualTheme = false;
 
   ThemeData get currentTheme => _currentTheme;
   bool get isDarkMode => _isDarkMode;
+  bool get isManualTheme => _isManualTheme;
 
-  void switchTheme() {
+  // Method to manually change the theme
+  void switchTheme() async {
     _isManualTheme = true;
     if (_isDarkMode) {
       _currentTheme = AppTheme.lightTheme();
@@ -21,25 +26,39 @@ class ThemeNotifier with ChangeNotifier {
       _isDarkMode = true;
     }
     notifyListeners();
+    await _saveThemePreference();
   }
 
-  void setLightTheme() {
+  // Method to apply the light theme
+  void setLightTheme() async {
     _isManualTheme = true;
     _currentTheme = AppTheme.lightTheme();
     _isDarkMode = false;
     notifyListeners();
+    await _saveThemePreference();
   }
 
-  void setDarkTheme() {
+  // Method to apply the dark theme
+  void setDarkTheme() async {
     _isManualTheme = true;
     _currentTheme = AppTheme.darkTheme();
     _isDarkMode = true;
     notifyListeners();
+    await _saveThemePreference();
   }
 
-  void resetManualTheme() {
-    _isManualTheme = false;
+  // Method for saving the theme in SharedPreferences
+  Future<void> _saveThemePreference() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(_themePreferenceKey, _isDarkMode);
   }
 
-  bool get isManualTheme => _isManualTheme;
+  // Method to load the theme saved by SharedPreferences
+  static Future<ThemeNotifier> loadThemeFromPreferences() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool isDarkMode = prefs.getBool(_themePreferenceKey) ?? false;
+    final ThemeData initialTheme =
+        isDarkMode ? AppTheme.darkTheme() : AppTheme.lightTheme();
+    return ThemeNotifier(initialTheme, isDarkMode);
+  }
 }
