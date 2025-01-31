7f26da470bc20bc03fa739985798d5d98090362b
ndenicolais <ndn21dev@gmail.com>
2025-01-31 11:13:56 +0100

    Added URL constants for Support Screen

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  static final Uri uriMail = Uri(
+    scheme: 'mailto',
+    path: 'ndn21dev@gmail.com',
+  );
+  static final Uri uriGithubProfile =
+      Uri.parse('https://ndenicolais.github.io/');
+  static final Uri uriGithubLink = Uri.parse('https://ndenicolais.github.io/');
+  static final Uri uriGithubDocumentation =
+      Uri.parse('https://github.com/ndenicolais/Shox/');
+}
