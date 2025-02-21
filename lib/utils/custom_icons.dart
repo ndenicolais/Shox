dcea394e73c96714bbdb0cf99534e9014d2b51c4
ndenicolais <ndn21dev@gmail.com>
2025-02-21 17:20:51 +0100

    Updated the Shoes's colors icons

---
+++ lib/utils/custom_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class ShoxIcons {
+  ShoxIcons._();
+
+  static const _kFontFam = 'ShoxIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData iconShoesPrimary =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData iconShoesSecondary =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
